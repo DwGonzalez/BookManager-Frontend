3575256a2dca3d7493761dd450413497f589e166
Darwin Gonzalez <darwinmanuel1@hotmail.com>
2025-02-20 04:52:50 -0400

    fix book interface

--- src/app/domain/models/book.interface.ts
+++ src/app/domain/models/book.interface.ts
@@ -1,8 +1,8 @@
 export interface Book {
-  id: number;
-  title: string;
-  description: string;
-  pageCount: number;
-  excerpt: string;
-  publishDate: string;
+  id?: number;
+  title?: string;
+  description?: string;
+  pageCount?: number;
+  excerpt?: string;
+  publishDate?: Date;
 }
